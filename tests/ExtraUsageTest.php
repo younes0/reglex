c197326d89e7de0a3763836674dc6c331d9d547c
Younès El Biache <younes.elbiache@gmail.com>
2015-12-04 16:00:56 +0100

    stuff

--- tests/ExtraUsageTest.php
+++ tests/ExtraUsageTest.php
@@ -19,33 +19,40 @@ class ExtraUsageTest extends \PHPUnit_Framework_TestCase
 
     public function testDccVisa()    
     {
-        $str1 = 'Vu la Constitution et notamment ses articles 76 et 77 ;';
-        $str2 = 'Vu l\'avis du Conseil d\'État en date du 16 juillet 2013, blabla la loi organique susvisée ;';
+        $string1 = 'Vu la Constitution et notamment ses articles 76 et 77 ;';
+        $string2 = 'Vu l\'avis du Conseil d\'État en date du 16 juillet 2013, blabla la loi organique susvisée ;';
 
-        $v = $this->extra->dccVisa($str1.PHP_EOL.$str2);
+        $v = $this->extra->dccVisa($string1.PHP_EOL.$string2);
 
-        $this->assertEquals($v[0], $str1);
-        $this->assertEquals($v[1], $str2);
+        $this->assertEquals($v[0], $string1);
+        $this->assertEquals($v[1], $string2);
     }
     
     public function testDccConsiderant()    
     {
-        $str1 = '1. Considérant que le blabla ;';
-        $str2 = '21. Considérant que blabla ;';
+        $string1 = '1. Considérant que le blabla ;';
+        $string2 = '21. Considérant que blabla ;';
 
-        $v = $this->extra->dccConsiderant($str1.PHP_EOL.$str2);
+        $v = $this->extra->dccConsiderant($string1.PHP_EOL.$string2);
 
-        $this->assertEquals($v[0], $str1);
-        $this->assertEquals($v[1], $str2);
+        $this->assertEquals($v[0], $string1);
+        $this->assertEquals($v[1], $string2);
     }
     
-    public function testdccOuCommentaireDccPremierParagraphe()
+    public function testDccOuCommentaireDccPremierParagraphe()
     {
+        $string1 = 'Le Conseil constitutionnel a été saisi le 28 janvier 2015, par le Premier ministre, dans les conditions prévues par le second alinéa de l\'article 37 de la Constitution, d\'une demande tendant à ce qu\'il se prononce sur la nature juridique du nombre « cinq » figurant au troisième alinéa de l\'article L. 758-1 du code de l\'éducation.';
 
+        $string2 = PHP_EOL.'LE CONSEIL CONSTITUTIONNEL, '.PHP_EOL.$string1;
+
+        $v = $this->extra->dccOuCommentaireDccPremierParagraphe($string1.$string2);
+
+        $this->assertEquals($v[0], $string1);
+        $this->assertEquals(count($v), 1);
     }
     
-    // public function testdccMembres()    
-    // {
-
-    // }
+    public function testDccMembres()    
+    {
+        $v = $this->extra->testdccMembres($string1);
+    }
 }
